f2be429d89cc8855af182a57253e945af74aec6e
MoSamir104 <mohammedsamiralklashany@gmail.com>
2025-11-28 17:58:31 +0200

    data validaiton competed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import CustomException
 from networksecurity.logging.logger import logging 
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DatatValidaiton
 import sys
 
 if __name__ == "__main__":
@@ -12,7 +13,13 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(data_ingestion_config=dataingestionconfig)
         logging.info("Initiate the data ingestion")
         Dataingetstionartifact = data_ingestion.initiate_data_ingetstion()
+        logging.info("data initiation completed")
         print(Dataingetstionartifact)
-        logging.info("Enter the try block")
+        data_validation_config = DataValidationConfig(trainingpiplineconfig)
+        data_validation = data_validation = DatatValidaiton(Dataingetstionartifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_vallidation_artifact = data_validation.initiate_data_validation()
+        logging.info("data validation completed")
+        print(data_vallidation_artifact)
     except Exception as e:
         raise CustomException(e, sys)
