6fca4f5431b748d7b313323bbc4be5f50eded280
MoSamir104 <mohammedsamiralklashany@gmail.com>
2025-11-29 13:33:21 +0200

    DataTransformationCommit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import CustomException
 from networksecurity.logging.logger import logging 
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DatatValidaiton
+from networksecurity.components.data_transformation import DataTransformation
 import sys
 
 if __name__ == "__main__":
@@ -21,5 +22,11 @@ if __name__ == "__main__":
         data_vallidation_artifact = data_validation.initiate_data_validation()
         logging.info("data validation completed")
         print(data_vallidation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpiplineconfig)
+        logging.info("data transformation started")
+        data_transformation = DataTransformation(data_vallidation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data transformation completed")
     except Exception as e:
         raise CustomException(e, sys)
